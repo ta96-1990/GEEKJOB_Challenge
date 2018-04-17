5d85413ecf11301f0b2ff75f19da8e66dc6a6c7e
ta96-1990 <38029946+ta96-1990@users.noreply.github.com>
2018-04-17 15:15:02 +0900

    java課題　クラス2　クラスの継承

---
+++ CampChallenge_java/013.クラス/Java_c_13_2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author honnd
+ */
+public class Java_c_13_2 extends Java_c_13 {
+     public static void main(String[] args){
+        Java_c_13 data = new Java_c_13();
+        data.name ="null";
+        data.num = 0;
+    }
+}
