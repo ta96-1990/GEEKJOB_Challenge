87d2f5d4581754b16658059a67cbdcf0c78f7943
ta96-1990 <38029946+ta96-1990@users.noreply.github.com>
2018-04-25 16:09:36 +0900

    java課題　標準クラス5　バイト数と文字数の取得

---
+++ CampChallenge_java/015.標準クラス/java_challenge15_5.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author honnd
+ */
+public class java_challenge15_5 {
+    public static void main(String[] args){
+        String name = "ほんだたくろう";
+        
+        System.out.println("バイト数"+name.getBytes().length);
+        System.out.println(name.length());
+    }
+}
