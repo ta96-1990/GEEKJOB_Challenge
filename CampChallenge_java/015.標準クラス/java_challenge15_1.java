0386ba6b47b9d04e016020c4bc68330564eedbf2
ta96-1990 <38029946+ta96-1990@users.noreply.github.com>
2018-04-25 13:36:33 +0900

    java課題　標準クラス1　タイムスタンプの作成

---
+++ CampChallenge_java/015.標準クラス/java_challenge15_1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Date;
+/**
+ *
+ * @author honnd
+ */
+public class java_challenge15_1 {
+ public static void main(String[] args){
+     Date day = new Date(2016,1,1,0,0,0);
+     System.out.println(day);
+     System.out.println(day.getTime());
+ }   
+}
