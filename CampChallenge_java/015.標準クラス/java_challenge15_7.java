5e607743fc3c9abb420ef53f5e556802e4f1500c
ta96-1990 <38029946+ta96-1990@users.noreply.github.com>
2018-04-25 16:51:44 +0900

    java課題　標準クラス7　文字の入れ替え

---
+++ CampChallenge_java/015.標準クラス/java_challenge15_7.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author honnd
+ */
+public class java_challenge15_7 {
+    public static void main(String[] args){
+        String diary ="きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        String replace1 = diary.replace("I", "い");
+        String replace2 = replace1.replace("U", "う");
+        System.out.print(replace2);
+    }
+    
+}
