4bcb8ae69b1fa6318342bb764ef6ecdb27059c3e
ta96-1990 <38029946+ta96-1990@users.noreply.github.com>
2018-04-26 11:11:42 +0900

    java課題　標準クラス9　ファイルから読み出しと表示

--- CampChallenge_java/015.標準クラス/java_challenge15_9.java
+++ CampChallenge_java/015.標準クラス/java_challenge15_9.java
@@ -3,11 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  *
  * @author honnd
  */
 public class java_challenge15_9 {
+    public static void main(String[] args) throws IOException{
+        File pr = new File("c:¥¥tmp¥¥pr.txt");
+        pr.createNewFile();
+        FileWriter fw = new FileWriter(pr);
+        fw.write("自己紹介の記述されたテキストファイルを作成してください。ファイルの作成は，File クラスなどを利用することによって行ってください。");
+        FileReader fr = new FileReader(pr);
+        BufferedReader br = new BufferedReader(fr);
+        fw.close();
+        System.out.print(br.readLine());
+        
     
 }
