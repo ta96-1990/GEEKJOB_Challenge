448cfd4053891512a43705e9d78d950d60e20c05
ta96-1990 <38029946+ta96-1990@users.noreply.github.com>
2018-04-26 10:59:35 +0900

    java課題　標準クラス8　ファイルの書き出しと保存

---
+++ CampChallenge_java/015.標準クラス/java_challenge15_8.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+/**
+ *
+ * @author honnd
+ */
+public class java_challenge15_8 {
+    public static void main(String[] args) throws IOException{
+        File pr = new File("c:¥¥tmp¥¥pr.txt");
+        pr.createNewFile();
+        FileWriter fw = new FileWriter(pr);
+        fw.write("自己紹介の記述されたテキストファイルを作成してください。ファイルの作成は，File クラスなどを利用することによって行ってください。");
+        FileReader fr = new FileReader(pr);
+        BufferedReader br = new BufferedReader(fr);
+        fw.close();
+        System.out.print(br.readLine());
+        
+    }
+    
+}
