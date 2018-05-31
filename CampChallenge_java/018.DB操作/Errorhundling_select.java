bdcc7ccb8fe10165ab9daa23b9f2111b15ac5535
ta96-1990 <38029946+ta96-1990@users.noreply.github.com>
2018-05-31 14:24:15 +0900

    DB操作　課題　 全情報の取得と表示

---
+++ CampChallenge_java/018.DB操作/Errorhundling_select.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import static java.lang.System.out;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author honnd
+ */
+public class Errorhundling_select extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        Connection con = null;
+        String url = "jdbc:mysql://localhost:3306/Challenge_db";
+        String user = "ta96";
+        String pass = "296019900918";
+        try (PrintWriter out = response.getWriter()) {
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                con = DriverManager.getConnection(url,user,pass);
+                Statement st = con.createStatement();
+                String sql = "select * from profiles";
+                ResultSet rs = st.executeQuery(sql);
+                while(rs.next()){
+                    int id = rs.getInt("profileID");
+                    String name = rs.getString("name");
+                    String tel = rs.getString("tel");
+                    int age = rs.getInt("age");
+                    String birth = rs.getString("birthday");
+                    out.println("<p>");
+                    out.println("ID:"+id+" 名前:"+name+" 連絡先:"+tel+" 年齢:"+age+" 誕生日:"+birth+"</p>");
+                }
+                rs.close();
+                st.close();
+            } catch (ClassNotFoundException e) {
+            } catch (SQLException e){
+
+            } catch (Exception e){
+
+            }finally{
+            try{
+                if(con !=null);{
+               con.close();
+            }
+            }catch(SQLException e){
+
+            }
+        }
+        }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
